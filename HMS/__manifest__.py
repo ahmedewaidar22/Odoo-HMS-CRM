a21b2a0a0ddf8b50e2310809bc89ce4f67e5fdac
ahmeddewaidar <ahmedewaidar22@gmail.com>
2022-02-12 03:12:25 +0200

    add

---
+++ HMS/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': ' HMS',
+    'summary': 'this is out new modle',
+    'description': """
+    1.
+    2.
+    3.
+    """,
+    'data': ['views/CreatePatient.xml',
+             'views/CreateDepartments.xml',
+             'views/crm.xml',
+             ],
+    'depends':['crm'],
+
+}
